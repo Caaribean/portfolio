52ce45de5ceb47574b317bfb2a4b07e667d2675a
Caaribean <klaudia.gnatkiewicz@gmail.com>
2023-03-10 11:17:41 +0100

    Change position of elements in the header and main-section for the dekstop view

--- components/header.js
+++ components/header.js
@@ -11,13 +11,14 @@ class Header extends HTMLElement {
           <h2 class="header__logo__txt">Agency</h2>
         </div>
         <nav class="header__navmenu">
-          <ul>
-            <li class="header__navmenu__txt"><a href="#">About</a></li>
-            <li class="header__navmenu__txt"><a href="#">Services</a></li>
-            <li class="header__navmenu__txt"><a href="#">Pricing</a></li>
-            <li class="header__navmenu__txt"><a href="#">Blog</a></li>
+          <ul class="header__navmenu__list">
+            <li><a class="header__navmenu__list__txt" href="aboutus.html">About</a></li>
+            <li><a class="header__navmenu__list__txt" href="services.html">Services</a></li>
+            <li><a class="header__navmenu__list__txt" href="#">Pricing</a></li>
+            <li><a class="header__navmenu__list__txt" href="blog.html">Blog</a></li>
           </ul>
         </nav>
+        <button class="header__btn--black"><a class="nav-text" href="contact.html">Contact</a></button>
         <div class="header__hamburger">
           <img class="header__hamburger__img" src="./images/hamburger.png" />
         </div>
